a532fdab533e9451258c016b296795cb4eebb219
YYG609 <yyg864@gmail.com>
2023-12-05 11:40:31 +0900

    TeamProject_KioskProgramming

---
+++ app/src/main/java/com/example/kioskprogramming/KioskProgramming.kt
@@ -0,0 +1,13 @@
+package com.example.kioskprogramming
+
+fun main(){
+//    [필요한 기능]
+//    요구사항1: 메인 메뉴판 화면
+//    : 메뉴 선택시 상세 메뉴화면으로 이동
+//    : 잘못된 번호 선택 시 예외처리
+//    : 프로그램 종료를 위한 번호 정의
+}
+
+class KioskProgramming {
+
+}
